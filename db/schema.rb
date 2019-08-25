64a598bbae39e47fcb00a09c4464bb9dac076b5b
Kosuke Saitou <kosuke.saito@bizreach.co.jp>
2019-08-25 22:14:53 +0900

    Revert "imgのカラム追加"
    This reverts commit 6780bcd93f0d52c4fb68390fc9ff6f1455df54f1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_124541) do
+ActiveRecord::Schema.define(version: 2019_08_13_105612) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "img"
   end
 
   create_table "comments", force: :cascade do |t|
